d990153d48fb1f25ab76cf844362c1af1a113f6e
Korkmatik <fahri.korkmaz@protonmail.ch>
2020-10-12 19:56:23 +0200

    Removed Django Project

--- web/fahrzeuge/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Fahrzeug
-
-
-admin.site.register(Fahrzeug)
-
--- web/veranstaltung/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = 'veranstaltung'
-urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('<int:id>', views.VeranstaltungsView.as_view(), name='detail'),
-]

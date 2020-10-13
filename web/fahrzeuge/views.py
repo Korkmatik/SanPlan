8edb0f13caaf659ff73cb3568bd9395f18703dee
Korkmatik <fahri.korkmaz@protonmail.ch>
2020-10-13 22:25:55 +0200

    Create get method for CreateView

--- web/fahrzeuge/views.py
+++ web/fahrzeuge/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 
 from .models import Fahrzeug
+from .controllers import VehicleController, VehicleTypeController
 
 
 class IndexView(View):
@@ -16,4 +17,19 @@ class IndexView(View):
                 'fahrzeuge': fahrzeuge,
                 'fahrzeugeActive': True,
             }
+        )
+
+
+class CreateView(View):
+
+    def get(self, request):
+        context = {
+            'vehicle_types': VehicleTypeController.get_all_vehicle_types(),
+            'states': VehicleController.get_vehicle_states(),
+        }
+
+        return render(
+            request,
+            'fahrzeuge/create.html',
+            context
         )

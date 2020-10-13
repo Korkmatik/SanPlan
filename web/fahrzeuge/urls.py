1d8e8aae11ea53575aa3dcee2e0dc79e557ac67a
Korkmatik <fahri.korkmaz@protonmail.ch>
2020-10-13 22:26:55 +0200

    Created route for CreateView

--- web/fahrzeuge/urls.py
+++ web/fahrzeuge/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'fahrzeuge'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('create', views.CreateView.as_view(), name='create'),
 ]

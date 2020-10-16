657f7f43776a2ec75c15ed91f25642e377c2acd1
Korkmatik <fahri.korkmaz@protonmail.ch>
2020-10-16 18:14:46 +0200

    Added EventTestCase

---
+++ web/veranstaltung/tests/test_controllers.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+from veranstaltung.controllers import VeranstaltungController
+
+
+class EventTestCase(TestCase):
+
+    def test_get_event_by_username_no_event(self):
+        pass
+

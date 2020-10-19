f2a56e6f86aa2fb3f3c55ec715e78370fb8ff9a3
Korkmatik <fahri.korkmaz@protonmail.ch>
2020-10-19 18:38:54 +0200

    Removed get_veranstaltung_by_username

--- web/veranstaltung/controllers.py
+++ web/veranstaltung/controllers.py
@@ -4,10 +4,6 @@ from .models import Veranstaltung
 class VeranstaltungController():
 
     @staticmethod
-    def get_veranstaltung_by_username(username):
-        return Veranstaltung.objects.filter(users__username=username).all()
-
-    @staticmethod
     def get_veranstaltung_by_id(id):
         return Veranstaltung.objects.get(id=id)
 

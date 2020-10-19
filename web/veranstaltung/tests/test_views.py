e13c09c8397776e350c8dd938d7df43efbb64482
Korkmatik <fahri.korkmaz@protonmail.ch>
2020-10-19 19:13:12 +0200

    Added tests for views

---
+++ web/veranstaltung/tests/test_views.py
@@ -0,0 +1,52 @@
+from typing import Tuple, List, Optional
+
+from django.test import TestCase, Client
+from django.urls import reverse
+
+from veranstaltung.tests.test_models import EventTestCase
+
+
+class EventViewTestCase:
+
+    @staticmethod
+    def setUp(url: str, args: Optional[List] = None) -> Tuple[str, Client]:
+
+        if args is None:
+            url = reverse('veranstaltung:' + url)
+        else:
+            url = reverse('veranstaltung:' + url, args=args)
+        client = Client()
+        return url, client
+
+    @staticmethod
+    def get_template_names(response) -> List[str]:
+        return [template.name for template in response.templates]
+
+
+class IndexViewTestCase(TestCase):
+
+    def setUp(self) -> None:
+        self.url, self.client = EventViewTestCase.setUp('index')
+
+    def test_get(self):
+        response = self.client.get(self.url)
+        template_names = EventViewTestCase.get_template_names(response)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('veranstaltung/index.html' in template_names)
+
+
+class VeranstaltungsViewTestCase(TestCase):
+
+    def setUp(self) -> None:
+        self.event = EventTestCase.create_event()[0]
+        self.event.save()
+
+        self.url, self.client = EventViewTestCase.setUp('detail', [self.event.id])
+
+    def test_get(self):
+        response = self.client.get(self.url)
+        template_names = EventViewTestCase.get_template_names(response)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('veranstaltung/detail.html' in template_names)

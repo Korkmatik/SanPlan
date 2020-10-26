3401877076b8138ba57cab0d1b5595aabf1f58f9
Korkmatik <fahri.korkmaz@protonmail.ch>
2020-10-26 19:45:09 +0100

    Added UserRepository

--- sanplan/src/main/java/local/korkmatik/SanPlan/repositories/UserRepository.java
+++ sanplan/src/main/java/local/korkmatik/SanPlan/repositories/UserRepository.java
@@ -1,4 +1,10 @@
 package local.korkmatik.SanPlan.repositories;
 
-public class UserRepository {
+import local.korkmatik.SanPlan.models.user.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, Integer> {
+
 }

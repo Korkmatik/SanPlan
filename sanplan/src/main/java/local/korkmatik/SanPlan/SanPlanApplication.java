34fd6dd3d410787592047860b9cfef21778d799a
Korkmatik <fahri.korkmaz@protonmail.ch>
2020-10-26 19:16:05 +0100

    Added Spring Project

---
+++ sanplan/src/main/java/local/korkmatik/SanPlan/SanPlanApplication.java
@@ -0,0 +1,13 @@
+package local.korkmatik.SanPlan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanPlanApplication.class, args);
+	}
+
+}

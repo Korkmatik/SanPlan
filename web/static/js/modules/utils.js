7ec63fe279e6ca2a3737df3562d8ff1d74dc68ba
Korkmatik <fahri.korkmaz@protonmail.ch>
2020-10-28 21:53:58 +0100

    Added toBase64

--- web/static/js/modules/utils.js
+++ web/static/js/modules/utils.js
@@ -20,3 +20,12 @@ export function launch_error_toast(message) {
     x.className = "show";
     setTimeout(function(){ x.className = x.className.replace("show", ""); }, 5000);
 }
+
+export function toBase64(file) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = error => reject(error);
+    });
+}

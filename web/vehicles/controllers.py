73ee4daa4337122a36e5a38d9788c2a6342c13bb
Korkmatik <fahri.korkmaz@protonmail.ch>
2020-11-07 00:08:15 +0100

    Added static method get_vehicle_by_id_or_404

--- web/vehicles/controllers.py
+++ web/vehicles/controllers.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 
 from .models import Vehicle, VehicleType
 
@@ -7,6 +8,10 @@ class VehicleController:
     @staticmethod
     def get_vehicle_states():
         return Vehicle.STATUS_CHOICES
+
+    @staticmethod
+    def get_vehicle_by_id_or_404(id: int):
+        return get_object_or_404(Vehicle, id=id)
 
 
 class VehicleTypeController:

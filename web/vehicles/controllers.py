f4ff65576c9effcd8103606740350382e98a3427
Korkmatik <fahri.korkmaz@protonmail.ch>
2020-11-07 11:10:22 +0100

    Added update_vehicle

--- web/vehicles/controllers.py
+++ web/vehicles/controllers.py
@@ -13,6 +13,17 @@ class VehicleController:
     def get_vehicle_by_id_or_404(id: int):
         return get_object_or_404(Vehicle, id=id)
 
+    @staticmethod
+    def update_vehicle(vehicle: Vehicle, data, image):
+        vehicle.type = VehicleType.objects.get(short=data['vehicle-type'])
+        vehicle.name = data['vehicle-name']
+        vehicle.license_plate = data['license-plate']
+        vehicle.radio_call_name = data['radio-call-name']
+        vehicle.image = image
+        vehicle.status = data['state']
+        vehicle.seats = data['seats']
+        vehicle.save()
+
 
 class VehicleTypeController:
 
